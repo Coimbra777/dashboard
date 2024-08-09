bbc1d221fe90c2f54e6426cc8ffe93fa0f8a1476
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-08-09 17:01:21 -0300

    first commit

---
+++ resources/assets/js/cms/components/UICpf.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" :name="name" :class="inputClass" :id="name" v-model="inputModel" v-mask="'###.###.###-##'"
+      :required="required">
+    <span v-if="errorMessage" class="help-block">
+      <strong>{{ errorMessage }}</strong>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name', 'value', 'required', 'class_name', 'error'],
+  data() {
+    return {
+      inputModel: ''
+    }
+  },
+  computed: {
+    errorMessage() {
+      return this.error ? this.error : '';
+    },
+    inputClass() {
+      return this.class_name + (this.error ? ' has-error' : '');
+    }
+  },
+  mounted() {
+    this.inputModel = this.value;
+  }
+}
+</script>
+
+<style scoped>
+.help-block {
+  color: rgba(100, 90, 90, 0.74);
+}
+</style>
